493e305c9ed295aa164221807a85e08cf62bb647
Mohamed Magdy <Eng.M.Ezzeldeen@gmail.com>
2025-02-22 15:38:50 +0200

    Update Mega-Footer.js

--- JS/Mega-Footer.js
+++ JS/Mega-Footer.js
@@ -151,17 +151,21 @@ let megaContactUs =
 
     <h4 class="mb-4 pb-4 border-bottom">Website :</h4>
     
-    <div class="d-flex justify-content-start">    
-        <button class="bg-transparent text-white border-0 p-0"
-              onclick="openFooterLink('eng-m-ma9dy.github.io/MAGDY/')"><img src="https://eng-m-ma9dy.github.io/Static-Files/IMG/Profile-Logo-MM-White.jpg"
-                class="footer_logo rounded-circle" alt=""></button>
-    </div>
+    <div class="d-flex justify-content-start">
+            <div class="d-flex justify-content-center mx-1">    
+                <button class="bg-transparent text-white border-0 p-0"
+                      onclick="openFooterLink('eng-m-ma9dy.github.io/MAGDY/')"><img src="https://eng-m-ma9dy.github.io/Static-Files/IMG/Profile-Logo-MM-White.jpg"
+                        class="footer_logo rounded-circle" alt=""></button>
+            </div>
 
-    <div class="d-flex justify-content-start">    
-        <button class="bg-transparent text-white border-0 p-0"
-              onclick="openFooterLink('eng-m-ma9dy.github.io/MAGDY/')"><img src="https://eng-m-ma9dy.github.io/Static-Files/IMG/Profile-Logo-IU-Black.jpg"
-                class="footer_logo rounded-circle" alt=""></button>
-    </div> 
+            <div class="d-flex justify-content-center mx-1">    
+                <button class="bg-transparent text-white border-0 p-0 border-white"
+                      onclick="openFooterLink('eng-m-ma9dy.github.io/MAGDY/')"><img src="https://eng-m-ma9dy.github.io/Static-Files/IMG/Profile-Logo-IU-Black.jpg"
+                        class="footer_logo rounded-circle" alt=""></button>
+            </div> 
+    
+    </div>
+    
     
   </div>
 
